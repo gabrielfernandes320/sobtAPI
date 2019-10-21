4c44d4c625479f1854ffd095ee63820580ad4583
Gabriel <gabrielfernandesfg@hotmail.com>
2019-10-21 03:40:59 -0300

    cors solution

--- src/main/java/com/sobt/controller/ServiceOrdersController.java
+++ src/main/java/com/sobt/controller/ServiceOrdersController.java
@@ -6,21 +6,16 @@ import com.sobt.ServiceOrder;
 import com.sobt.repository.ServiceOrderRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1")
 public class ServiceOrdersController {
 
     @Autowired
     private ServiceOrderRepository serviceOrderRepository;
+
 
     @GetMapping("/serviceOrders")   // GET Method for reading operation
     public List<ServiceOrder> getAllServiceOrders() {
